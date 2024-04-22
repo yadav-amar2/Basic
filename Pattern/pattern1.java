b8fc025460fb764e2e9c244666c869e8019aa07d
yadav-amar2 <amar550yadav@gmail.com>
2024-04-23 00:47:09 +0530

    first commit

---
+++ Pattern/pattern1.java
@@ -0,0 +1,10 @@
+class Pattern1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();  // To move to the next line after printing a row of asterisks
+        }
+    }
+}
